f4507ccd779b70eb191dfa1dc1c74d8c1d70b3aa
vandersongc <vandersongc@gmail.com>
2025-10-27 18:26:11 -0300

    v2

--- exercicios3.py
+++ exercicios3.py
@@ -3,8 +3,11 @@
 '''
 print('='*50)
 print('Atividade 3')
+print('Saldo em conta: Bradesco')
 class ContaBancaria:
-    def __init__(self, saldo):
+    def __init__(self, banco, conta, saldo):
+            self.banco = banco 
+            self.conta = conta 
             self.__saldo = saldo #atributo privado
 
     def depositar(self,valor):
@@ -19,8 +22,8 @@ class ContaBancaria:
     def mostrar_saldo(self):
         print(f'Saldo atual:R$ {self.__saldo}.')
 
-contaBancaria1 = ContaBancaria('Itaú','Conta Corrente')
-contaBancaria2 = ContaBancaria("Bradesco", "Poupança")
+contaBancaria1 = ContaBancaria('Itaú','Conta Corrente', 40)
 contaBancaria1.depositar()
 contaBancaria1.depositar()
+contaBancaria1.sacar()
 print('='*50)
